51c5c73ab159c35bbd7ab98e379105194c2be93c
Do Nhat Duc <duc21102001@gmail.com>
2024-06-19 00:16:27 +0700

    Done functione change table and detail bill

---
+++ RestaurantManage/Controllers/DetailBillController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManage.Models;
+
+namespace RestaurantManage.Controllers;
+
+public class DetailBillController : Controller
+{
+    private QuanLyNhaHangContext _context = new QuanLyNhaHangContext(); 
+    // GET
+    public IActionResult Index(int? id)
+    {
+        string username = HttpContext.Session.GetString("UserName");
+        string role = HttpContext.Session.GetString("Role");
+        id = id ?? 0;
+        var bill = _context.Bills.Include(e => e.BillInfos).ThenInclude(e => e.IdFoodNavigation).SingleOrDefault(e => e.Id == id);
+        var tables = _context.TableFoods.Where(e => e.Status == "0").ToList();
+        
+        ViewBag.username = username;
+        ViewBag.role = role;
+        ViewBag.bill = bill;
+        ViewBag.tables = tables;
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult ChangeTable(int idBill, int idTableChange)
+    {
+        if (idTableChange != 0)
+        {
+            var bill = _context.Bills.SingleOrDefault(e => e.Id == idBill);
+            bill.IdTable = idTableChange;
+            _context.Update(bill);
+            _context.SaveChanges();
+        }
+        return RedirectToAction("Index", "History");
+    }
+}
--- RestaurantManage/Controllers/HistoryController.cs
+++ RestaurantManage/Controllers/HistoryController.cs
@@ -9,17 +9,59 @@ public class HistoryController : Controller
     private QuanLyNhaHangContext _context = new QuanLyNhaHangContext(); 
 
     // GET
-    public IActionResult Index()
+    public IActionResult Index(int? page)
     {
+        page = page ?? 1;
         string username = HttpContext.Session.GetString("UserName");
         string role = HttpContext.Session.GetString("Role");
         List<Bill> bills = new List<Bill>();
-        bills = _context.Bills.Include(e => e.IdTableNavigation).OrderByDescending(e => e.DateCheckIn).ToList();
+        List<Bill> totalBills = new List<Bill>();
+        
+        totalBills = _context.Bills.ToList();
+        int countPage = (int)Math.Ceiling((double)totalBills.Count / 8);
+        
+        
+        bills = _context.Bills.Include(e => e.IdTableNavigation).Skip((page.Value - 1) * 10).Take(10).OrderByDescending(e => e.DateCheckIn).ToList();
        
+        ViewBag.countPage = countPage;
+        ViewBag.page = page;
         ViewBag.username = username;
         ViewBag.role = role;
         ViewBag.bills = bills;
         
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult CancelBill(int id)
+    {
+        var bill = _context.Bills.SingleOrDefault(e => e.Id == id);
+        bill.Status = 2;
+        _context.Update(bill);
+        _context.SaveChanges();
+        
+        var table = _context.TableFoods.SingleOrDefault(e => e.Id == bill.IdTable);
+        table.Status = "0";
+        _context.Update(table);
+        _context.SaveChanges();
+        
+        return RedirectToAction("Index", "History");
+    }
+    
+    [HttpPost]
+    public IActionResult ConfirmBill(int id)
+    {
+        var bill = _context.Bills.SingleOrDefault(e => e.Id == id);
+        bill.Status = 1;
+        bill.DateCheckOut = DateTime.Now;
+        _context.Update(bill);
+        _context.SaveChanges();
+        
+        var table = _context.TableFoods.SingleOrDefault(e => e.Id == bill.IdTable);
+        table.Status = "0";
+        _context.Update(table);
+        _context.SaveChanges();
+        
+        return RedirectToAction("Index", "History");
     }
 }
